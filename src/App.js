ee643d6290210ecc9cec7142f252eae8be3cb3f5
Valkyrie <ksenia.korsun@gmail.com>
2019-07-26 11:56:56 +0300

    React App Component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Header from './Header/Header'
+import Main from './Main/Main'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div className="App PageWrapper">
+      <Header />
+      <Main />
     </div>
-  );
+    );
 }
 
 export default App;
